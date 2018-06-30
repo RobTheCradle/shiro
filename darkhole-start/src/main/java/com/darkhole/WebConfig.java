369dadd3daff66fea70c4a66be1e5b1e0972836f
RobTheCradle <37287428+RobTheCradle@users.noreply.github.com>
2018-06-30 12:20:44 +0800

    冲突文件

---
+++ darkhole-start/src/main/java/com/darkhole/WebConfig.java
@@ -0,0 +1,17 @@
+package com.darkhole;
+
+
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+public class WebConfig extends WebMvcConfigurerAdapter {
+    /**
+     * 配置静态访问资源
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/resource/static");
+        super.addResourceHandlers(registry);
+    }
+}
